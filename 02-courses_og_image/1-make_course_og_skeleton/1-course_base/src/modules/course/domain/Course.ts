406bf14e2405585850a1e81773bb36b810948623
Rafa Gómez <rgomezcasas@gmail.com>
2023-12-04 23:44:31 +0100

    feat(2-1): add example

--- 02-courses_og_image/1-make_course_og_skeleton/1-course_base/src/modules/course/domain/Course.ts
+++ 02-courses_og_image/1-make_course_og_skeleton/1-course_base/src/modules/course/domain/Course.ts
@@ -9,4 +9,15 @@ export class Course {
 		public readonly slug: string,
 		public readonly color: string,
 	) {}
+
+	hexColor(): string {
+		const colors: { [key: string]: string } = {
+			green: "#3cff64",
+			yellow: "#fef878",
+			pink: "#e0a4bf",
+			violet: "#7026f4",
+		};
+
+		return colors[this.color] || "#3cff64";
+	}
 }
--- 02-courses_og_image/1-make_course_og_skeleton/2-dynamic_image/src/app/cursos/[slug]/opengraph-image.tsx
+++ 02-courses_og_image/1-make_course_og_skeleton/2-dynamic_image/src/app/cursos/[slug]/opengraph-image.tsx
@@ -30,17 +30,35 @@ export default async function Image({ params }: { params: { slug: string } }) {
 		(
 			<div
 				style={{
-					display: "flex",
-					alignItems: "center",
-					justifyContent: "center",
+					fontSize: 16,
+					background: "#7026f4",
+					color: "#ffffff",
 					width: "100%",
 					height: "100%",
-					fontSize: "5rem",
-					backgroundColor: "tomato",
-					color: "white",
+					display: "flex",
 				}}
 			>
-				{course.title}
+				<div
+					style={{
+						display: "flex",
+						background: course.hexColor(),
+						width: "30%",
+						height: "100%",
+						alignItems: "center",
+						justifyContent: "center",
+					}}
+				>
+					<img
+						src={`https://codely.com${course.sticker}`}
+						alt="Curse sticker"
+						style={{
+							width: 250,
+						}}
+					/>
+				</div>
+				<div style={{ display: "flex", width: "70%", fontSize: "4rem", padding: "4rem" }}>
+					{course.emoji} {course.title}
+				</div>
 			</div>
 		),
 		{

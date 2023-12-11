5c5b0d99f673064cb67b4ad0c039479227fdacbd
Rafa Gómez <rgomezcasas@gmail.com>
2023-12-11 16:14:41 +0100

    feat(5-2): add example

--- 05-og_image_other_projects/2-nestjs/src/app.controller.ts
+++ 05-og_image_other_projects/2-nestjs/src/app.controller.ts
@@ -1,12 +1,48 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import satori from 'satori';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<string> {
+    return await satori(
+      {
+        type: 'div',
+        props: {
+          style: {
+            backgroundColor: 'black',
+            display: 'flex',
+            width: '100%',
+            height: '100%',
+            alignItems: 'center',
+            justifyContent: 'center',
+          },
+          children: {
+            type: 'h1',
+            props: {
+              style: { color: 'white', fontSize: '5rem' },
+              children: 'Codely',
+            },
+          },
+        },
+      },
+      {
+        width: 1200,
+        height: 630,
+        fonts: [
+          {
+            name: 'Comic Mono',
+            data: readFileSync(join(process.cwd(), 'public', 'ComicMono.ttf')),
+            weight: 400,
+            style: 'normal',
+          },
+        ],
+      },
+    );
   }
 }
